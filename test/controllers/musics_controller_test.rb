6d80f2454e94097951d3eec7ef35260fd6348381
ajcksyxd <a2413414649@gmail.com>
2020-12-02 19:00:08 +0900

    事前準備

--- test/controllers/musics_controller_test.rb
+++ test/controllers/musics_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class MusicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get musics_index_url
     assert_response :success
   end
-
 end
