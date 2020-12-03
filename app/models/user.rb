d37b13044d004734bac26072b8f2f3d80099817a
ajcksyxd <a2413414649@gmail.com>
2020-12-03 18:48:58 +0900

    エラーメッセージ表示と日本語化対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z+\d]+\z/i.freeze
+
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
+  
 end
