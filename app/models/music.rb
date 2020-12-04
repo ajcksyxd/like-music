34f63fa7f089003dbef6cf8cf1e61527b21ef7b6
ajcksyxd <a2413414649@gmail.com>
2020-12-04 23:34:20 +0900

    ActiveStorageを導入

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,3 +1,12 @@
 class Music < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :artist
+    validates :image
+    validates :audio
+    validates :introduction
+  end
+  
   belongs_to :user
+  has_one_attached :image
 end
